73cbd140b0ef0944f355dfe33e2c139f257df2a6
Antonio Lentini <antle718@gmail.com>
2020-06-30 13:55:02 +0200

    Update run_kinetic_inference_cellrep.sh

--- kinetics/run_kinetic_inference_cellrep.sh
+++ kinetics/run_kinetic_inference_cellrep.sh
@@ -2,7 +2,7 @@
 
 ## tpm
 parallel -j 1 ./txburstML.py ::: input/tpm/*.csv
-mv *.pkl kinetics/output/tpm
+mv *.pkl output/tpm
 parallel -j 10 ./pkl_to_tsv.py ::: output/tpm/*.pkl
 
 ./split_kinetics.R output/tpm/ output/tpm/split/postimplantation 2,3,4,1
