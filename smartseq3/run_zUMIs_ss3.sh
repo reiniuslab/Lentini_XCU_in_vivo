214f2b6056ff884497e91e11fb608ec4522454c7
Antonio Lentini <antle718@gmail.com>
2020-06-30 13:59:08 +0200

    Create run_zUMIs_ss3.sh

---
+++ smartseq3/run_zUMIs_ss3.sh
@@ -0,0 +1,6 @@
+zUMIs/zUMIs-master.sh -y mESC_NEW.yaml
+zUMIs/zUMIs-master.sh -y mESC_clone5_diff.yaml
+
+# from https://github.com/sandberg-lab/Smart-seq3/tree/master/allele_level_expression
+Rscript get_variant_overlap_CAST.R --yaml mESC_NEW.yaml --vcf /huxley.hdd/projects/smartseq3/CAST.SNPs.superset.ENSEMBL.vcf.gz
+Rscript get_variant_overlap_CAST.R --yaml mESC_clone5_diff.yaml --vcf /huxley.hdd/projects/smartseq3/CAST.SNPs.superset.ENSEMBL.vcf.gz
